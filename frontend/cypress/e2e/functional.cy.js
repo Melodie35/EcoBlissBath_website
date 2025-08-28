4bb2fe6186c308d4abd00ab56ea8a5a43a633765
Melodie <melodie.migault@gmail.com>
2025-08-29 00:54:39 +0200

    functional.cy.js finished

--- frontend/cypress/e2e/functional.cy.js
+++ frontend/cypress/e2e/functional.cy.js
@@ -1,27 +1,27 @@
 //Connexion
-// describe('connexion', () => {
+describe('connexion', () => {
 
-//     beforeEach(() => {
-//         cy.visit('http://localhost:4200/#/')
-//         cy.getBySel('nav-link-login').click()
-//     })
+    beforeEach(() => {
+        cy.visit('http://localhost:4200/#/')
+        cy.getBySel('nav-link-login').click()
+    })
 
-//     it('gets the connexion form', () => {
-//         cy.getBySel('login-input-username').should('be.visible')
-//         cy.getBySel('login-input-password').should('be.visible')
-//         cy.getBySel('login-submit').should('be.visible')
-//     })
+    it('gets the connexion form', () => {
+        cy.getBySel('login-input-username').should('be.visible')
+        cy.getBySel('login-input-password').should('be.visible')
+        cy.getBySel('login-submit').should('be.visible')
+    })
 
-//     it('should be connected', () => {
-//         cy.getBySel('login-input-username').type('test2@test.fr')
-//         cy.getBySel('login-input-password').type('testtest')
-//         cy.getBySel('login-submit').click()
+    it('should be connected', () => {
+        cy.getBySel('login-input-username').type('test2@test.fr')
+        cy.getBySel('login-input-password').type('testtest')
+        cy.getBySel('login-submit').click()
 
-//         cy.getBySel('nav-link-cart').should('be.visible')
-//         cy.getBySel('nav-link-logout').should('be.visible')
+        cy.getBySel('nav-link-cart').should('be.visible')
+        cy.getBySel('nav-link-logout').should('be.visible')
 
-//     })
-// })
+    })
+})
 
 
 //Panier
@@ -44,11 +44,21 @@ describe('Add to the cart function', () => {
 
     //le stock doit être supérieur à 1 pour pouvoir être ajouté
     it('checks products stock before adding to cart', () => {
-        cy.wait('@getProduct').then(({response}) => {
-            const stock = response.body.availableStock
-            cy.getBySel('detail-product-add').should(stock > 1 ? 'not.be.disabled' : 'be.disabled')
-        })    
-    })
+        cy.wait('@getProduct').then((intercept) => {
+            let stock = intercept.response.body.availableStock
+            console.log(stock)
+
+            cy.intercept('PUT', 'http://localhost:8081/orders/add').as('addCart')
+            cy.getBySel('detail-product-add').click()
+            if (stock > 1){
+                cy.wait('@addCart').its('response.statusCode').should('eq', 200)
+            } else {
+                cy.wait(1000).then(() => {
+                    cy.get('@addCart.all').should('have.length', 0)
+                })
+            }
+        })     
+    })   
 
     // vérifiez que le produit a été ajouté au panier
     it('checks the product was added to the cart', () => {
@@ -65,6 +75,7 @@ describe('Add to the cart function', () => {
                 .then((val) => expect(+val).to.be.at.least(1))
         })
     })
+
 
     //vérifiez que le stock a enlevé le nombre de produits qui sont dans le panier
     it('checks the stock after adding a product to the cart', () => {
@@ -104,7 +115,13 @@ describe('Add to the cart function', () => {
             cy.getBySel('detail-product-quantity')
                 .clear()
                 .type('-1')
-            cy.getBySel('detail-product-add').should('be.disabled')
+
+            cy.intercept('PUT', 'http://localhost:8081/orders/add').as('addCart')
+            cy.getBySel('detail-product-add').click()
+            cy.wait(1000).then(() => {
+                    cy.get('@addCart.all').should('have.length', 0)
+                })
+            
         }) 
     })
 
@@ -114,11 +131,36 @@ describe('Add to the cart function', () => {
             cy.getBySel('detail-product-quantity')
                 .clear()
                 .type('21')
-            cy.getBySel('detail-product-add').should('be.disabled')
+
+            cy.intercept('PUT', 'http://localhost:8081/orders/add').as('addCart')
+            cy.getBySel('detail-product-add').click()
+            cy.wait(1000).then(() => {
+                    cy.get('@addCart.all').should('have.length', 0)
+                })
         }) 
     })
 
-    // it('checks cart content after adding a product', () => {})
+
+    // vérification du contenu du panier via l’API
+    it('checks cart content after adding a product', () => {
+        cy.intercept('PUT', 'http://localhost:8081/orders/add').as('addCart')
+        cy.intercept('GET', 'http://localhost:8081/orders').as('getOrders')
+
+        cy.wait('@getProduct').then(() => {
+            cy.getBySel('detail-product-add').click()
+        })
+
+        cy.wait('@addCart').then((intercept) => {
+            cy.getBySel('cart-line').should('have.length.greaterThan', 0)
+            expect(intercept.response.statusCode).to.eq(200)
+            expect(intercept.response.body.orderLines).to.exist
+            expect(intercept.response.body.orderLines[0].product).to.deep.include({ id: 3, name: "Sentiments printaniers", description: "Savon avec une formule douce à base d’huile de framboise, de citron et de menthe qui nettoie les mains efficacement sans les dessécher.", price: 60 })
+            expect(intercept.request.body).to.deep.include({ product: 3 })
+            })
+        
+        cy.wait('@getOrders').its('response.statusCode').should('eq', 200)
+
+    })
 
     // Vérifiez la présence du champ de disponibilité du produit
     it('checks the Stock field of the product', () => {
