634cdf92985c74df930487ed4d722b3421a818de
Melodie <melodie.migault@gmail.com>
2025-08-29 03:07:17 +0200

    api.cy.js and fixtures done

---
+++ frontend/cypress/e2e/api.cy.js
@@ -0,0 +1,185 @@
+//GET
+
+//Requête sur les données confidentielles d'un utilisateur avant connexion --> erreur
+describe('cart access without connexion', () => {
+    it('should return an error', () => {
+        cy.request({
+                method: 'GET',
+                url: 'http://localhost:8081/orders',
+                failOnStatusCode: false
+            }).then((response) => {
+                expect(response.status).to.eq(401)
+            })   
+    })
+})
+
+//Requête de la liste des produits du panier
+describe('cart access when connected', () => {
+    let token
+
+    before(() => {
+        cy.fixture('users').then((user) => {
+            cy.request({
+                method: 'POST',
+                url: 'http://localhost:8081/login',
+                body: user.utilisateurConnu
+            }).then((response => {
+                token = response.body.token
+            }))
+        })        
+    })
+    
+    it('should return the product list of the cart', () => {
+        cy.request({
+            method: 'GET',
+            url: 'http://localhost:8081/orders',
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            expect(response.body.orderLines).to.be.an('array')                
+        })   
+    })
+})
+
+//Requête d’une fiche produit spécifique
+describe('specific product', () => {
+    it('should return the product details and its id', () => {
+        cy.request({
+            method: 'GET',
+            url: 'http://localhost:8081/products/3'
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            expect(response.body.id).to.eq(3)
+            expect(response.body).to.have.keys('id', 'name','availableStock', 'skin', 'aromas', 'ingredients', 'description', 'price', 'picture', 'varieties')
+        })
+    })
+})
+
+/********************************************************/
+//POST
+
+//Vérifier le login via http://localhost:8081/login
+describe('login', () => {
+    beforeEach(() => {
+        cy.fixture('users').as('users')
+    })
+
+    it('should return error 401 with unknown user', function () {
+        const user = this.users.utilisateurInconnu
+
+        cy.request({
+                method: 'POST',
+                url: 'http://localhost:8081/login',
+                body: user,
+                failOnStatusCode: false
+            }).then((response) => {
+                expect(response.status).to.eq(401)
+                expect(response.body).to.deep.include({ message: "Invalid credentials." })
+            })        
+    })
+
+    it('should return 200 with known user', function () {
+        const user = this.users.utilisateurConnu
+
+        cy.request({
+                method: 'POST',
+                url: 'http://localhost:8081/login',
+                body: user
+            }).then((response) => {
+                expect(response.status).to.eq(200)
+                expect(response.body).to.have.property('token')
+            })        
+    })
+})
+
+//Ajouter un produit au panier
+describe('http://localhost:8081/orders/add', () => {
+    let token
+
+    beforeEach(() => {
+        cy.fixture('users').then((user) => {
+            cy.request({
+                method: 'POST',
+                url: 'http://localhost:8081/login',
+                body: user.utilisateurConnu
+            }).then((response => {
+                token = response.body.token
+            }))
+        })
+        
+        cy.fixture('products').as('products')
+    })
+
+    //un produit disponible
+    it('should add a product available', function () {
+        const product = this.products.disponible
+
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:8081/orders/add',
+            body: product,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            expect(response.body.orderLines).to.be.an('array')                
+        })
+    })
+
+    //un produit en rupture de stock
+    it('should not add a product unavailable', function () {
+        const product = this.products.rupture
+
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:8081/orders/add',
+            body: product,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then((response) => {
+            expect(response.status).to.not.eq(200)               
+        })
+    })
+})
+
+
+//Ajouter un avis
+describe('http://localhost:8081/reviews', () => {
+    let token
+
+    before(() => {
+        cy.fixture('users').then((user) => {
+            cy.request({
+                method: 'POST',
+                url: 'http://localhost:8081/login',
+                body: user.utilisateurConnu
+            }).then((response => {
+                token = response.body.token
+            }))
+        })
+    })
+
+    it('should add a review', () => {
+         cy.request({
+            method: 'POST',
+            url: 'http://localhost:8081/reviews',
+            body: {
+                "title": "Test",
+                "comment": "Test sur Cypress",
+                "rating": 4
+            },
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then((response) => {
+            console.log(response)
+            expect(response.status).to.eq(200)
+            expect(response.body).to.have.property('author')                
+        })
+    })
+})
+
